73321ca2b2b8f3273e33507d4714664591e1bd66
johnfreyes <jhonfer96@hotmail.com>
2021-09-28 23:12:54 -0500

    image user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,22 +19,38 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Vehicles')
       ),
       body: _getBody(),
-      drawer: _getMechanicMenu(),
+      drawer: widget.token.user.userType == 0
+      ? _getMechanicMenu() 
+      : _getCustomerMenu(),
     );
   }
 
   Widget _getBody() {
     return Container(
       margin: EdgeInsets.all(30),
-      child: Center(
-        child: Text(
-          'Bienvenid@ ${widget.token.user.fullName}',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(150),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/vehicles-logo.png'),
+              image: NetworkImage(widget.token.user.imageFullPath),
+              height: 300,
+              fit: BoxFit.cover,
+            )
           ),
-        ),
-      ),
+          SizedBox(height: 30),
+          Center(
+            child: Text(
+            'Bienvenid@ ${widget.token.user.fullName}',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+        ,
+          )]
+      )
     );
   }
 
@@ -45,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           DrawerHeader(
             child: Image(
-              image: AssetImage('assets/vehicles_logo.png'),
+              image: AssetImage('assets/vehicles-logo.png'),
             )
           ),
           ListTile(
@@ -71,6 +87,47 @@ class _HomeScreenState extends State<HomeScreen> {
           ListTile(
             leading: Icon(Icons.people),
             title: const Text('Usuarios'),
+            onTap: () { },
+          ),
+          Divider(
+            color: Colors.black, 
+            height: 2,
+          ),
+          ListTile(
+            leading: Icon(Icons.face),
+            title: const Text('Editar Perfil'),
+            onTap: () { },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: const Text('Cerrar Sesión'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen()
+                )
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getCustomerMenu() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Image(
+              image: AssetImage('assets/vehicles-logo.png'),
+            )
+          ),
+          ListTile(
+            leading: Icon(Icons.two_wheeler),
+            title: const Text('Mis vehículos'),
             onTap: () { },
           ),
           Divider(
