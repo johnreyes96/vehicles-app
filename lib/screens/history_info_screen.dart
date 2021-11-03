d12f7b0ac57bb61a4cc75af541dd64d965edfd2b
johnfreyes <jhonfer96@hotmail.com>
2021-11-02 23:36:14 -0500

    history info screen

--- lib/screens/history_info_screen.dart
+++ lib/screens/history_info_screen.dart
@@ -2,44 +2,45 @@ import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:vehicles_app/components/loader_component.dart';
 import 'package:vehicles_app/helpers/api_helper.dart';
+import 'package:vehicles_app/models/detail.dart';
+
 import 'package:vehicles_app/models/history.dart';
+import 'package:vehicles_app/models/procedure.dart';
 import 'package:vehicles_app/models/response.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/models/user.dart';
 import 'package:vehicles_app/models/vehicle.dart';
-import 'package:vehicles_app/screens/history_info_screen.dart';
-import 'package:vehicles_app/screens/history_screen.dart';
-import 'package:vehicles_app/screens/vehicle_screen.dart';
+import 'package:vehicles_app/screens/detail_screen.dart';
 
-class VehicleInfoScreen extends StatefulWidget {
+class HistoryInfoScreen extends StatefulWidget {
   final Token token;
   final User user;
   final Vehicle vehicle;
+  final History history;
   
-  VehicleInfoScreen({ required this.token, required this.user, required this.vehicle });
+  HistoryInfoScreen({ required this.token, required this.user, required this.vehicle, required this.history });
 
   @override
-  _VehicleInfoScreenState createState() => _VehicleInfoScreenState();
+  _HistoryInfoScreenState createState() => _HistoryInfoScreenState();
 }
 
-class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
+class _HistoryInfoScreenState extends State<HistoryInfoScreen> {
   bool _showLoader = false;
-  late Vehicle _vehicle;
+  late History _history;
 
   @override
   void initState() {
     super.initState();
-    _vehicle = widget.vehicle;
+    _history = widget.history;
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${_vehicle.brand.description} ${_vehicle.line} ${_vehicle.plaque}'),
+        title: Text('${widget.vehicle.brand.description} ${widget.vehicle.line} ${widget.vehicle.plaque}'),
       ),
       body: Center(
         child: _showLoader
@@ -48,66 +49,92 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _goAddHistory(History(
-          date: '',
-          dateLocal: '',
-          details: [],
-          detailsCount: 0,
+        onPressed: () => _goAddDetail(Detail(
           id: 0,
-          mileage: 0,
-          remarks: '',
-          total: 0,
-          totalLabor: 0,
-          totalSpareParts: 0
+          procedure: Procedure(id: 0, description: '', price: 0),
+          laborPrice: 0,
+          sparePartsPrice: 0,
+          totalPrice: 0,
+          remarks: ''
         ))
       ),
     );
   }
 
-  void _goHistory(History history) async {
-    String? result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HistoryInfoScreen(
-        token: widget.token,
-        user: widget.user,
-        vehicle: _vehicle,
-        history: history,
-      ))
-    );
-    if (result == 'yes') {
-      await _getVehicle();
-    }
-  }
-
-  void _goAddHistory(History history) async {
-    String? result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HistoryScreen(
-        token: widget.token,
-        user: widget.user,
-        vehicle: _vehicle,
-        history: history,
-      ))
-    );
-    if (result == 'yes') {
-      await _getVehicle();
-    }
-  }
-
   Widget _getContent() {
     return Column(
       children: <Widget>[
-        _showVehicleInfo(),
+        _showHistoryInfo(),
         Expanded(
-          child: _vehicle.histories.length == 0 ? _noContent() : _getListView()
+          child: _history.details.length == 0 ? _noContent() : _getListView()
         )
       ],
     );
   }
 
-  Widget _showVehicleInfo() {
+  void _goAddDetail(Detail detail) async {
+    String? result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailScreen(
+        token: widget.token,
+        user: widget.user,
+        vehicle: widget.vehicle,
+        history: widget.history,
+        detail: detail,
+      ))
+    );
+    if (result == 'yes') {
+      await _getHistory();
+    }
+  }
+
+  Future<Null> _getHistory() async {
+    setState(() {
+      _showLoader = true;
+    });
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+
+      await showAlertDialog(
+        context: context,
+        title: 'Error',
+        message: 'Verifica que estés conectado a internet.',
+        actions: <AlertDialogAction>[
+          AlertDialogAction(key: null, label: 'Aceptar')
+        ]
+      );
+      return;
+    }
+
+    Response response = await ApiHelper.getHistory(widget.token, widget.history.id.toString());
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (!response.isSuccess) {
+      await showAlertDialog(
+        context: context,
+        title: 'Error',
+        message: response.message,
+        actions: <AlertDialogAction>[
+          AlertDialogAction(key: null, label: 'Aceptar')
+        ]
+      );
+      return;
+    }
+
+    setState(() {
+      _history = response.result;
+    });
+  }
+
+  _showHistoryInfo() {
     return Container(
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(5),
@@ -118,7 +145,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
               ClipRRect(
                 borderRadius: BorderRadius.circular(5),
                 child: CachedNetworkImage(
-                  imageUrl: _vehicle.imageFullPath,
+                  imageUrl: widget.vehicle.imageFullPath,
                   errorWidget: (context, url, err) => Icon(Icons.error),
                   fit: BoxFit.cover,
                   height: 100,
@@ -172,7 +199,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.vehicleType.description, 
+                              widget.vehicle.vehicleType.description, 
                               style: TextStyle(
                                 fontSize: 14,
                               ),
@@ -189,7 +216,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.brand.description,
+                              widget.vehicle.brand.description,
                               style: TextStyle(
                                 fontSize: 14,
                               ),
@@ -206,7 +233,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.model.toString(),
+                              widget.vehicle.model.toString(),
                               style: TextStyle(
                                 fontSize: 14,
                               ),
@@ -223,7 +250,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.plaque,
+                              widget.vehicle.plaque,
                               style: TextStyle(
                                 fontSize: 14,
                               ),
@@ -240,7 +267,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.line,
+                              widget.vehicle.line,
                               style: TextStyle(
                                 fontSize: 14
                               )
@@ -257,7 +284,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.color,
+                              widget.vehicle.color,
                               style: TextStyle(
                                 fontSize: 14
                               )
@@ -274,7 +301,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.remarks == null ? 'N/A' : _vehicle.remarks!,
+                              widget.vehicle.remarks == null ? 'N/A' : widget.vehicle.remarks!,
                               style: TextStyle(
                                 fontSize: 14
                               )
@@ -291,7 +318,7 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
                               )
                             ),
                             Text(
-                              _vehicle.historiesCount.toString(),
+                              widget.vehicle.historiesCount.toString(),
                               style: TextStyle(
                                 fontSize: 14
                               )
@@ -311,173 +338,14 @@ class _VehicleInfoScreenState extends State<VehicleInfoScreen> {
   }
 
   Widget _noContent() {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.all(20),
-        child: Text(
-          'El vehículo no tiene historias registradas',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold
-          ),
-        ),
-      )
-    );
+    return Container();
   }
 
   Widget _getListView() {
-    return RefreshIndicator(
-      onRefresh: _getVehicle,
-      child: ListView(
-        children: _vehicle.histories.map((e) {
-          return Card(
-            child: InkWell(
-              onTap: () => _goHistory(e),
-              child: Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(5),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                  '${DateFormat('yyyy-MM-dd').format(DateTime.parse(e.dateLocal))}',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Text(
-                                      '${e.mileage} Kms',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    SizedBox(width: 5),
-                                    Text(
-                                      e.remarks == null ? 'N/A' : e.remarks!,
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Text(
-                                      'Mano de obra: ${NumberFormat.currency(symbol: '\$').format(e.totalLabor)}',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ]
-                                ),
-                                 Row(
-                                  children: <Widget>[
-                                    Text(
-                                      'Repuestos: ${NumberFormat.currency(symbol: '\$').format(e.totalSpareParts)}',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ]
-                                ),
-                                 Row(
-                                  children: <Widget>[
-                                    Text(
-                                      'Total: ${NumberFormat.currency(symbol: '\$').format(e.total)}',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ]
-                                ),
-                              ]
-                            )
-                          ]
-                        )
-                      )
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      size: 40
-                    )
-                  ]
-                )
-              )
-            )
-          );
-        }).toList(),
-      )
-    );
+    return Container();
   }
 
-  void _goEdit() async {
-    String? result = await Navigator.push(
-      context, 
-      MaterialPageRoute(
-        builder: (context) => VehicleScreen(
-          token: widget.token, 
-          user: widget.user,
-          vehicle: _vehicle,
-        )
-      )
-    );
-    if (result == 'yes') {
-      await _getVehicle();
-    }
-  }
-
-  Future<Null> _getVehicle() async {
-    setState(() {
-      _showLoader = true;
-    });
-
-    var connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.none) {
-      setState(() {
-        _showLoader = false;
-      });
-
-      await showAlertDialog(
-        context: context,
-        title: 'Error',
-        message: 'Verifica que estés conectado a internet.',
-        actions: <AlertDialogAction>[
-          AlertDialogAction(key: null, label: 'Aceptar')
-        ]
-      );
-      return;
-    }
-
-    Response response = await ApiHelper.getVehicle(widget.token, _vehicle.id.toString());
-
-    setState(() {
-      _showLoader = false;
-    });
-
-    if (!response.isSuccess) {
-      await showAlertDialog(
-        context: context,
-        title: 'Error',
-        message: response.message,
-        actions: <AlertDialogAction>[
-          AlertDialogAction(key: null, label: 'Aceptar')
-        ]
-      );
-      return;
-    }
-
-    setState(() {
-      _vehicle = response.result;
-    });
+  Widget _goEdit() {
+    return Container();
   }
 }
