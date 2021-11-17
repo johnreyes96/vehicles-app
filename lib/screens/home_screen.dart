603f0ef66891588c1a4f15ab235eb6998e39b2c7
johnfreyes <jhonfer96@hotmail.com>
2021-11-17 06:54:47 -0500

    integration with phone app and whatsapp app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/screens/document_types_screen.dart';
@@ -11,6 +12,7 @@ import 'package:vehicles_app/screens/user_info_screen.dart';
 import 'package:vehicles_app/screens/user_screen.dart';
 import 'package:vehicles_app/screens/users_screen.dart';
 import 'package:vehicles_app/screens/vehicle_types_screen.dart';
+import 'package:whatsapp_unilink/whatsapp_unilink.dart';
 
 class HomeScreen extends StatefulWidget {
   final Token token;
@@ -36,39 +38,81 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _getBody() {
-    return Container(
-      margin: EdgeInsets.all(30),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(150),
-            child: CachedNetworkImage(
-              imageUrl: widget.token.user.imageFullPath,
-              errorWidget: (context, url, err) => Icon(Icons.error),
-              fit: BoxFit.cover,
-              height: 300,
-              width: 300,
-              placeholder: (context, url) => Image(
-                image: AssetImage('assets/vehicles_logo.png'),
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+              child: CachedNetworkImage(
+                imageUrl: widget.token.user.imageFullPath,
+                errorWidget: (context, url, err) => Icon(Icons.error),
                 fit: BoxFit.cover,
                 height: 300,
                 width: 300,
+                placeholder: (context, url) => Image(
+                  image: AssetImage('assets/vehicles_logo.png'),
+                  fit: BoxFit.cover,
+                  height: 300,
+                  width: 300,
+                )
               )
-            )
-          ),
-          SizedBox(height: 30),
-          Center(
-            child: Text(
-              'Bienvenid@ ${widget.token.user.fullName}',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
+            ),
+            SizedBox(height: 30),
+            Center(
+              child: Text(
+                'Bienvenid@ ${widget.token.user.fullName}',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
               ),
             ),
-          )
-        ]
-      )
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Llamar al taller'),
+                SizedBox(width: 10,),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    color: Colors.blue,
+                    child: IconButton(
+                      icon: Icon(Icons.call, color: Colors.white,),
+                      onPressed: () => launch("tel://3223114620"), 
+                    ),
+                  ),
+                )
+              ],
+            ),       
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Enviar mensaje al taller'),
+                SizedBox(width: 10,),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    color: Colors.green,
+                    child: IconButton(
+                      icon: Icon(Icons.insert_comment, color: Colors.white,),
+                      onPressed: () => _sendMessage(), 
+                    ),
+                  ),
+                )
+              ],
+            ),       
+          ],
+        )
+      ),
     );
   }
 
@@ -239,5 +283,13 @@ class _HomeScreenState extends State<HomeScreen> {
         builder: (context) => LoginScreen()
       )
     );
+  }
+
+  void _sendMessage() async {
+    final link = WhatsAppUnilink(
+      phoneNumber: '3223114620',
+      text: 'Hola soy ${widget.token.user.fullName} cliente del taller'
+    );
+    await launch('$link');
   }
 }
